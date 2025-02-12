efd9d6007e6017d8886fcc90d9bfa7fbb3f339e7
Hlanganani <hlangananimpungose@gmail.com>
2025-02-12 11:14:24 +0200

    Restructured the code according to Act, Arrange and Assert

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,34 +1,82 @@
 import org.example.Main;
 import org.junit.Test;
-import static java.awt.AWTEventMulticaster.add;
-import static org.example.Main.multiply;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
     @Test
     public void testAddingTwoNumbers() {
-        assertEquals(30, Main.add(15, 15));
-        assertEquals(-5, Main.add(-3, -2));
+        // Arrange
+        int num1 = 15;
+        int num2 = 15;
+
+        // Act
+        int result = Main.add(num1, num2);
+
+        // Assert
+        assertEquals(30, result);
+
+        // Arrange
+        num1 = -3;
+        num2 = -2;
+
+        // Act
+        result = Main.add(num1, num2);
+
+        // Assert
+        assertEquals(-5, result);
     }
 
     @Test
     public void testAddingManyNumbers() {
-        assertEquals(15, Main.add(1, 2, 3, 4, 5));
-        assertEquals(20, Main.add(11, 9));
-        assertEquals(-3, Main.add(-1, -2));
+        // Arrange
+        int[] numbers1 = {1, 2, 3, 4, 5};
+
+        // Act
+        int result = Main.add(numbers1);
+
+        // Assert
+        assertEquals(15, result);
+
+        // Arrange
+        int[] numbers2 = {11, 9};
+
+        // Act
+        result = Main.add(numbers2);
+
+        // Assert
+        assertEquals(20, result);
+
+        // Arrange
+        int[] numbers3 = {-1, -2};
+
+        // Act
+        result = Main.add(numbers3);
+
+        // Assert
+        assertEquals(-3, result);
     }
+
     @Test
     public void testMultiplyingTwoNumbers() {
-        assertEquals(8, multiply(2, 4));
-        assertEquals(-3, multiply(-1, 3));
-    }
+        // Arrange
+        int num1 = 2;
+        int num2 = 4;
 
+        // Act
+        int result = Main.multiply(num1, num2);
 
-    @Test
-    public void testMultiplyingMultipleNumbers() {
-        assertEquals(120, multiply(1, 2, 3, 4, 5));
-        assertEquals(3, multiply(1, 3));
-        assertEquals(-3, multiply(-1, 3));
+        // Assert
+        assertEquals(8, result);
+
+        // Arrange
+        num1 = -1;
+        num2 = 3;
+
+        // Act
+        result = Main.multiply(num1, num2);
+
+        // Assert
+        assertEquals(-3, result);
     }
 }
